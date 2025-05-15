880a35139d845817f17e61173ba79af7dbe5b4c9
carlos <robertorsilva.krlos@gmail.com>
2025-05-15 11:53:39 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { Sidebar } from "@/components/sideBar";
+
 export default function Home() {
-  return <div className="flex flex-1r">Olá Mundo</div>;
+  return <div className="flex flex-1r">
+    <Sidebar/>
+  </div>;
 }
