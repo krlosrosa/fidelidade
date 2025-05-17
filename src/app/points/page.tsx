b6b4a46bfe42b6dfb90245124d1af22a18a0cba0
carlos <robertorsilva.krlos@gmail.com>
2025-05-16 23:06:35 -0300

    first commit

--- src/app/points/page.tsx
+++ src/app/points/page.tsx
@@ -1,12 +1,17 @@
-'use client'
+"use client";
 import { SupabaseHttpClient } from "@/infra/supa-base-http";
 import { useUser } from "@clerk/nextjs";
 import { RemoteAddPoints } from "@/data/usecases/remote-add-points";
 import AdicionarPontosPage from "./outros";
+import { Suspense } from "react";
 
 export default function PagePoints() {
-    const { user } = useUser();
-  const novo = new SupabaseHttpClient(user?.id)
-  const repository = new RemoteAddPoints('loyalty_points', novo)
-  return <AdicionarPontosPage addPoints={repository} />
+  const { user } = useUser();
+  const novo = new SupabaseHttpClient(user?.id);
+  const repository = new RemoteAddPoints("loyalty_points", novo);
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <AdicionarPontosPage addPoints={repository} />
+    </Suspense>
+  );
 }
